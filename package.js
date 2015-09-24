aedf8046f8ff0c5a398ee1b87589bb7c1d366408
Max Bruchmann <bruchmann@signal7.de>
2015-09-24 18:18:23 +0200

    update to latest cron and monq

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'planettraining:workers',
   summary: 'Spawn headless worker meteor processes to work on async jobs',
-  version: '2.1.0',
+  version: '2.1.1',
   git: 'https://github.com/Differential/meteor-workers'
 });
 
@@ -9,7 +9,7 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
 
   Npm.depends({
-    monq: '0.3.1'
+    monq: '0.3.2'
   });
 
   api.use([
@@ -17,7 +17,7 @@ Package.onUse(function(api) {
     'mongo',
     'random',
     'differential:cluster@1.0.1',
-    'percolate:synced-cron@1.1.1',
+    'percolate:synced-cron@1.3.0',
     'underscorestring:underscore.string@3.2.2'
   ], 'server');
 
